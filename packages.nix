90763c44da00ac8f5fda7045ecc64472dd32adfa
System administrator <root@absolon.lan>
2017-11-21 15:02:35 +0200

    some changes

---
+++ packages.nix
@@ -0,0 +1,38 @@
+pkgs: with pkgs; [
+
+    abduco abiword acpi anki arandr asciinema aws-auth awscli bazaar bind binutils blueman bmon bridge-utils cmus cnijfilter2 compton ctags cups-bjnp curl cvs cvs_fast_export darcs deluge desktop_file_utils
+    dhcp dhcpcd di dmenu2 debootstrap dnsmasq dropbox dunst dvtm emacs evince exiv2 file firefoxWrapper fuse fzf gcc gimp git glib glxinfo global gnumake gnupg google-chrome gpm graphviz gtk2 gtypist
+    hdparm hicolor_icon_theme hipchat hplip rpm-ostree htop i3lock i3status 
+    bedup duperemove fdupes
+    emacs gitFull
+    iftop inkscape iomelt iptables 
+    irssi isync iw jdk jnettop jq leiningen libreoffice libsysfs libva lr lsof lxc lynx man-pages mc mercurialFull 
+    mkpasswd mongodb-tools mosh mpv msmtp mtr ncdu ncurses neomutt networkmanagerapplet nix-prefetch-scripts nix-repl nmap notmuch nq nssTools openocd openssl openvswitch pagemon pass pavucontrol pciutils peco 
+#    pinentry pkgconfig playerctl powertop psmisc python2Full python2Packages python36Full pythonPackages qt5.qtbase ranger reptyr ripgrep rofi rpm rtags rxvt_unicode rxvt_unicode_with-plugins scrot
+    shared_mime_info silver-searcher skype socat sshfsFuse sshpass stalonetray stunnel sublime3 subversion sxhkd sxiv taffybar tango-icon-theme taskwarrior termite texstudio tmux torbrowser tree tweak 
+#    unclutter unzip urlview usbutils vanilla-dmz vdpauinfo vifm vis vmtouch vnstat w3m-full watchman weechat wget winetricks wineUnstable wmctrl xautolock xclip xe xidel xlsfonts xorg xsel xss-lock 
+    zathura zip zsh
+   patchelf nixUnstable file nix-repl wget
+    ack adobe-reader ansible ansible2 ant apparmor-pam apparmor-parser apparmor-profiles apparmor-utils arduino asciinema aspell aspellDicts.en aspellDicts.fr atom autoconf axel babeltrace bc bedup bind bridge-utils bundix
+    calibre cargo cdrkit citrix_receiver clang cloc cloud-init cntlm containerd corkscrew davmail dia dmidecode docbook5 docbook_xml_xslt docker docker-machine docker-machine-kvm dos2unix duperemove ebtables
+    eclipses.eclipse-platform evince expect fasd ffmpeg filezilla firefox fop freemind freerdp gcc6 gdb gimp gitAndTools.gitFull gnome3.cheese gnome3.gconf gnome3.networkmanager_openconnect gnome3.pomodoro gnucash gnumake
+    gnupg go2nix google-chrome google-cloud-sdk gradle graphviz gtk-recordmydesktop guile
+     haskellPackages.hledger
+     icedtea_web  idea.pycharm-community iftop imagemagick inkscape kubernetes
+    languagetool
+    # ledger
+    # libguestfs libinput libmysql libpcap libproxy libreoffice libvirt libxslt libyaml lsh lsof ltrace lttng-tools lttng-ust maven meld mercurial midori minikube mosquitto mpv nasm ncdu neovim nethogs 
+    nfs-utils nix-prefetch-git nodejs nodePackages.gulp nss ntopng openconnect openjdk openssl oraclejdk8 packer pandoc paprefs pavucontrol pciutils pdftk php php70Packages.composer pidgin-with-plugins pipelight pkgconfig
+    #plantuml platformio playonlinux popfile proxychains psmisc pwgen
+    # pypi2nix python27Packages.libvirt python27Packages.pip python2Full python35 python35Packages.docker_compose python35Packages.libvirt python35Packages.paho-mqtt
+    # python35Packages.pip python35Packages.virtualenv pythonPackages.dopy pythonPackages.ipython pythonPackages.markupsafe pythonPackages.pyaml  pythonPackages.yamllint pythonPackages.yapf
+    # qpdfview 
+    #rfkill rsync ruby rustracer s3cmd screen scribus shellcheck simple-scan simplescreenrecorder skype slack spotify sqldeveloper sshpass steam stow subversionClient sysdig system-config-printer tcpdump terminator
+    # testdisk transmission_gtk tree unzipNLS vagrant virtmanager virt-viewer visualvm vlc weechat wineStaging winetricks wireshark-gtk xdg-user-dirs zeal zsh
+     ddate  devilspie2 evince google-chrome
+     hplip most mosh elinks links lynx screen tmux
+
+
+]
+
+
