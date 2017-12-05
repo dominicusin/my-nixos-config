d7159fac454d27c4fc4b5421144d95652937f711
Domini Montessori <root@absolon.lan>
2017-12-05 21:39:37 +0200

    some more changes

--- configuration.nix
+++ configuration.nix
@@ -1,463 +1,85 @@
-{ config, lib, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
+{ config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-    require =   [     ./mount.nix    ];
 
   # Use the GRUB 2 boot loader.
-  boot = {
-      consoleLogLevel = 7; #4
-      vesa = true;
-      tmpOnTmpfs = true;
-      cleanTmpDir = true;
-      extraTTYs = [ "tty8" "tty9" ];
-      supportedFilesystems = [ "bcachefs" "btrfs" "cifs" "exfat" "ext" "f2fs" "glusterfs" "jfs" "nfs" "ntfs" "reiserfs" "unionfs-fuse" "vboxsf" "vfat" "xfs" "zfs" "hfsplus" ];
-      kernelParams = [ "video=1920x1080" ];
-      kernelModules = [ "snd-seq" "snd-rawmidi" "kvm-intel" "r8169" "snd_hda_intel" "exfat" "exfat-nofuse" "msr" "coretemp" ];
-      blacklistedKernelModules = [ "snd_pcsp" "b43" "bcma" "bcma-pci-bridge" ];
-      #kernelPackages = pkgs.linuxPackages_latest; #pkgs.linux_grsec_server_latest; # pkgs.linuxPackages_4_11; #pkgs.linuxPackages_4_4;
-      #kernelPatches = [ pkgs.kernelPatches.ubuntu_fan_4_4 ];
-      extraModulePackages = with config.boot.kernelPackages; [ sysdig ];
-      kernel.sysctl = {
-          "fs.inotify.max_user_watches"   = 1048576;   # default:  8192# 524288; # Note that inotify watches consume 1kB on 64-bit machines.
-          "fs.inotify.max_user_instances" =    1024;   # default:   128
-          "fs.inotify.max_queued_events"  =   32768;   # default: 16384
-          "dev.cdrom.autoclose" = 0;
-          "kernel.parameters.consoleblank" = 0;
-          "kernel.pax.softmode" = 0;
-          "kernel.perf_event_max_sample_rate" = 30000;
-          "kernel.shmall" = 2097152;
-          "kernel.shmmax" = 33554432;
-          "kernel.sysrq" = 1;
-          "net.core.rmem_max" = 50000000;
-          "net.core.wmem_max" = 1048576;
-          "net.ipv4.ip_forward" = 1;
-          "net.ipv4.conf.all.accept_redirects" = 0;
-          "net.ipv4.conf.all.send_redirects" = 0;
-          "net.ipv4.conf.default.accept_redirects" = 0;
-          "net.ipv4.conf.default.send_redirects" = 0;
-          "net.ipv4.tcp_congestion_control" = "yeah";
-          "net.ipv4.tcp_fastopen" = 1;
-          "net.ipv4.tcp_slow_start_after_idle" = 0;
-          "net.ipv6.conf.all.accept_redirects" = 0;
-          "net.ipv6.conf.default.accept_redirects" = 0;
-          "vm.swappiness" = 90;
-          "vm.overcommit_memory" = 1;
-      };
-      initrd = {
-          kernelModules = [ "fbcon" "ohci_hcd" "ehci_hcd" "pata_amd" "sata_nv" "usb_storage"  "kvm-intel" "tun" "virtio" ];
-          availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "virtio_balloon" "virtio_blk" "virtio_pci" "virtio_ring" ];
-      };
-      loader = {
-          systemd-boot.enable = true;
-          timeout = 4;
-          grub = {
-              enable = true;
-              version = 2;
-              #efiSupport = true;
-              #efiInstallAsRemovable = true;
-              device = "nodev";   # "/dev/sda"; # or "nodev" for efi only # Define on which hard drive you want to install Grub.
-              extraEntries = ''
-                  if [ -f  ''${config_directory}/custom.cfg ]; then
-                      source ''${config_directory}/custom.cfg
-                 elif [ -z "''${config_directory}" -a -f  ''$prefix/custom.cfg ]; then
-                      source ''$prefix/custom.cfg;
-                 else
-                      source custom.cfg;
-                 fi
-              '';
-          };
-          efi = {
-              efiSysMountPoint = "/boot/efi";
-              canTouchEfiVariables = true;
-          };
-      };
-  };
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking = {
-      hostName = "absolon";
-      hostId = "a8c0b902";
-      nat = {
-          enable = true;
-          internalInterfaces = ["ve-+"];
-          externalInterface = "wlp4s0";
-      };
-      networkmanager.enable = lib.mkForce true;
-      networkmanager.insertNameservers = [ "8.8.8.8" "8.8.4.4" ];
-      nameservers = [ "8.8.8.8" "8.8.4.4" ];
-      timeServers = [ "178.17.160.12" "194.54.161.214" "212.59.0.2" "31.131.0.254" "80.96.120.251" "80.96.120.253" ];
-      wireless.enable = lib.mkForce false;
-      enableIPv6 = true;
-      firewall = {
-         enable = false;
-         allowPing = true;
-         #allowedTCPPorts = [ ... ];
-         #allowedUDPPorts = [ ... ];
-      };
-  };
-
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Select internationalisation properties.
-  i18n = {
-     consoleFont = "cyr-sun16";
-     consoleKeyMap = "ru";
-     defaultLocale = "ru_RU.UTF-8";
-     consoleUseXkbConfig = false;
-  };
-
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
 
   # Set your time zone.
-  time.timeZone = "Europe/Tiraspol";
+  # time.timeZone = "Europe/Amsterdam";
 
-  fonts = {
-    enableFontDir = true;
-    enableCoreFonts = true;
-    enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      corefonts
-      fantasque-sans-mono
-      inconsolata
-      unifont
-      ubuntu_font_family
-      noto-fonts
-      symbola
-    ];
-  };
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "devicemapper";
-  virtualisation.libvirtd.enable = true;
-  virtualisation.virtualbox.host.enable = false;
-  #virtualisation.virtualbox.host.enableHardening = true;
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
 
-  security.sudo.enable = true;
-  security.chromiumSuidSandbox.enable = true;
-  security.sudo.wheelNeedsPassword = false;
-  security.sudo.configFile="
-       root    ALL=(ALL) ALL
-       domini  ALL=(ALL) NOPASSWD: ALL
-  ";
-  security.pam.loginLimits = [
-      { domain = "root"; type = "-"; item = "memlock"; value = "unlimited"; }
-      { domain = "*"; type = "soft"; item = "nproc"; value = "65000"; }
-      { domain = "*"; type = "hard"; item = "nproc"; value = "1000000"; }
-      { domain = "*"; type = "-"; item = "nofile"; value = "1048576"; }
-      { domain = "*"; item = "nofile"; type = "-"; value = "999999"; }
-      { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
-      { domain = "@audio"; item = "rtprio"; type = "-"; value = "99"; }
-      { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
-      { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
-  ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  hardware = {
-      enableRedistributableFirmware = true;
-      cpu.intel.updateMicrocode = true;
-      cpu.amd.updateMicrocode = false;
-      facetimehd.enable = true;
-      opengl.enable = true;
-      opengl.driSupport = true;
-      opengl.driSupport32Bit = true;
-      opengl.extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
-      opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
-      bluetooth.enable = true;
-      sane = {
-          enable = true;
-          extraBackends = [ pkgs.hplipWithPlugin ];
-      };
-      pulseaudio = {
-            enable = true;
-            package = pkgs.pulseaudioFull.override { jackaudioSupport = true; };
-            support32Bit = true;
-            daemon.config = {
-                 flat-volumes = "no";
-            };
-      };
-  };
+  # List services that you want to enable:
 
-  programs = {
-      adb.enable = true;
-      bash = {
-         enableCompletion = true;
-         interactiveShellInit = ''
-                HISTFILESIZE=
-                HISTSIZE=
-                shopt -s histappend
-                HISTCONTROL=ignoreboth:erasedups
-                PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-         '';
-         promptInit = ''
-                export PS1="\[\e[32m\]\u\[\e[m\]@\[\e[32m\]\H\[\e[m\]:\[\e[34;40m\]\w\[\e[m\]\\$ "
-         '';
-         shellAliases = { ssh = "TERM=xterm-256color ssh"; };
-      };
-      bcc.enable = true;
-      browserpass.enable = true;
-      cdemu.enable = true;
-      chromium.enable = true;
-      command-not-found.enable = true;
-      fish.enable = true;
-      fish.vendor.completions.enable = true;
-      fish.vendor.config.enable = true;
-      fish.vendor.functions.enable = true;
-      ssh.startAgent = false;
-      gnupg = {
-          dirmngr.enable = true;
-          agent = {
-              enable = true;
-              enableSSHSupport = true;
-              enableBrowserSocket = true;
-              enableExtraSocket = true;
-          };
-      };
-      gphoto2.enable = true;
-      info.enable = true;
-      java.enable = true;
-      kbdlight.enable = true;
-      light.enable = true;
-      man.enable = true;
-      mosh.enable = true;
-      mtr.enable = true;
-      npm.enable = true;
-      oblogout.enable = true;
-      qt5ct.enable = true;
-      slock.enable = true;
-      spacefm.enable = true;
-      sway.enable = true;
-      sysdig.enable = true;
-      thefuck.enable = true;
-      tmux.enable = true;
-      wireshark.enable = true;
-      #xonsh.enable = true;
-      zsh.enable = true;
-      zsh.enableAutosuggestions = true;
-      zsh.enableCompletion = true;
-      zsh.ohMyZsh.enable = true;
-      zsh.syntaxHighlighting.enable = true;
-  };
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-  environment= {
-      systemPackages = import ./packages.nix pkgs;
-      variables = {
-              TERMINFO_DIRS = "/run/current-system/sw/share/terminfo";
-              ASPELL_CONF = "dict-dir /run/current-system/sw/lib/aspell";
-              EDITOR = pkgs.lib.mkOverride 0  "emacsclient -c";
-              BROWSER = pkgs.lib.mkOverride 0 "firefox";
-      };
-      extraInit = ''
-               dmesg -D
-      '';
-      etc = {
-          gitconfig.text = ''
-               [user]
-                  email = transgregorial@gmail.com
-                  name = Domini Montessori
-               [pull]
-                  rebase = true
-               [color]
-                  ui = auto
-               [push]
-                  default = simple
-               [merge]
-                  conflictstyle = diff3
-         '';
-         "bashrc.local".text = ''
-               dmesg -D
-         '';
-         "stack/config.yaml".text = ''
-                  templates:
-                     params:
-                        author-email: transgregorial@gmail.com
-                        author-name: Domini Montessori
-                        github-username: dominicusin
-                     nix::
-                        enable: true
-         '';
-    };
-  };
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
 
-  powerManagement = {
-     enable = true; cpuFreqGovernor = "performance";
-  };
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.extraUsers.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
 
-  services = {  # List services that you want to enable:
-    openssh.enable = true;  # Enable the OpenSSH daemon.
-    nixosManual.showManual = true;
-    gpm.enable = true;
-    kbfs.enable = true;
-    acpid.enable = true;
-    dnsmasq.enable = false;
-    gnome3.tracker.enable = false;
-    mpd = {
-          enable = true;
-          user = "domini";
-          group = "users";
-          musicDirectory = "/home/domini/Music";
-          dataDir = "/home/domini/.mpd";
-          extraConfig = ''
-              audio_output {
-                    type    "pulse"
-                    name    "Local MPD"
-                    server  "127.0.0.1"
-              }
-      '';
-    };
-    redis =   {
-          enable = false;
-          unixSocket = "/tmp/redis.sock";
-    };
-    syncthing = {
-        enable = true;
-        useInotify = true;
-        user = "domini";
-        dataDir = "/home/domini/.syncthing";
-        openDefaultPorts = true;
-    };
-    printing = {
-        enable = true; # Enable CUPS to print documents.
-        drivers = [ pkgs.hplipWithPlugin ];
-    };
-    xserver = {   # Enable the X11 windowing system.
-       enable = true;
-       #autorun = true;
-       layout = "us,ru(winkeys)";
-       enableCtrlAltBackspace = true;
-       libinput.enable = true;
-       xkbModel="geniuscomfy2";
-       xkbOptions = "grp:alt_shift_toggle,grp:win_switch,terminate:ctrl_alt_bksp,grp_led:scroll,altwin:menu";
-       videoDrivers = [ "intel" ];
-       deviceSection = ''
-              Option     "Accel"                    "True"
-       '';
-       desktopManager = {   # Enable  Desktop Environment.
-          #slim.enable = false;
-          enlightenment.enable = true; #e19.enable = true;
-          #gnome3.debug
-          gnome3.enable = true;
-          gnome3.extraGSettingsOverridePackages = [ pkgs.gnome3.nautilus ];
-          #gnome3.extraGSettingsOverrides
-          #gnome3.sessionPath = [ pkgs.gnome3.gpaste ];
-          #kde5 = true;
-          #kodi.enable = true; #xbmc
-          #lumina.enable = true;
-          #lxqt.enable = true;
-          mate.enable = true;
-          #maxx.enable = true;
-          plasma5.enable = true;
-          plasma5.enableQt4Support = true;
-          #wallpaper.combineScreens
-          #wallpaper.mode
-          xfce.enable = true;
-          xfce.enableXfwm = true;
-          #xfce.extraSessionCommands
-          xfce.noDesktop = false;
-          xfce.screenLock = "xscreensaver";
-          xfce.thunarPlugins = [ pkgs.xfce.thunar-archive-plugin pkgs.xfce.thunar_volman pkgs.xfce.thunar-dropbox-plugin ];
-          xterm.enable = false;
-          default = "Enlightenment"; #"xfce";"plasma5";"gnome3";"mate";
-       };
-       displayManager = {
-          auto.enable = false; auto.user = "domini";
-          #gdm.enable = false; gdm.autoLogin.enable = true; gdm.autoLogin.user = "domini"; 
-          #sddm.enable = false; sddm.autoLogin.enable = true; sddm.autoLogin.user = "domini";
-          #slim.enable = false; slim.autoLogin.enable = true; slim.autoLogin.user = "domini";
-          lightdm.enable = true; lightdm.autoLogin.enable = true; lightdm.autoLogin.user = "domini";
-       };
-       windowManager = {
-	  afterstep.enable = true;
-	  awesome.enable = true;
-	  bspwm.enable = true;
-	  dwm.enable = true;
-	  exwm.enable = true;
-	  exwm.enableDefaultConfig = true;
-	  fluxbox.enable = true;
-	  fvwm.enable = true;
-	  herbstluftwm.enable = true;
-	  i3.enable = true;
-	  icewm.enable = true;
-	  jwm.enable = true;
-	  #metacity.enable = true;
-	  mwm.enable = true;
-	  notion.enable = true;
-	  openbox.enable = true;
-	  pekwm.enable = true;
-	  qtile.enable = true;
-	  ratpoison.enable = true;
-	  sawfish.enable = true;
-	  spectrwm.enable = true;
-	  #stumpwm.enable = true;
-	  twm.enable = true;
-	  windowlab.enable = true;
-	  windowmaker.enable = true;
-	  wmii.enable = true;
-	  xmonad.enable = true;
-	  xmonad.enableContribAndExtras = true;
-       };
-   };
-   compton = {
-      #  enable          = true;
-      fade            = true;
-      inactiveOpacity = "0.9";
-      shadow          = true;
-      fadeDelta       = 4;
-   };
-  };
-
-  users.mutableUsers = true;
-  users.extraUsers.domini = { # Define a user account. Don't forget to set a password with ‘passwd’.
-     name = "domini";
-     isNormalUser = true;
-     uid = 1000;
-     description = "Domini Montessori";
-     group = "users";
-     extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal"  "vboxuser" "docker" ];
-     home = "/home/domini";
-     createHome = true;
-     useDefaultShell = true;
-     openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3NzaC1kc3MAAACBAOhcMJyuEf4AgS/LFwC23A/bcsGOkM33Mdoba5BXLHFE7UK62FteESwYNvY7oeSkJbsLzdpx6ntZmq5dNiWHCboqwtnzpVqL/PC0MGwLjuOuV43/k8+xYy2p2bJHb/mzuV9ew1sheK7F5f8LASkBGNiu5CR5puSxsnAaj8Bzi6GPAAAAFQDpO3+76Pj2I+STO7+afJMpht371QAAAIAHumBbnFiweZFHb7sqVphL7e1e35A09bzCzHh7SHAWQ817lkfM+LdeX2rTAxaufL2g7RBn10R4OyFbsTFiNpTo4KOsoPjeerOpJe4rR03gkXNWO1aOwx9kWJ5IjC7DgH6N+j7Oz7jXU9cGMa95QpN3UMCuDTnyQhozahP3gCEA+AAAAIBw2rcyQA6koR3XGkSq1XY/1rPZKPGFoCjSFf1R+OyNp8zkQ09rM9payU9nyirR8HOy5j1+y3F2e/5Sf9dpBfk0+bsqgwUCu3YoKl7uq8TROIU+eIwZKaytq/cVBaup5poee0GR4kn/dX4tV6qrh3cDIFl/SgUgkxJdunRb9VZtSQ== domini@absolon.hitech.local.prv" ];
-  };
-
-  system.copySystemConfiguration = true;
-  system.autoUpgrade.enable = true; 
-  system.stateVersion = "18.03"; #"17.09"; # "17.03"; # The NixOS release to be compatible with for stateful data such as databases.
-  system.autoUpgrade.channel = https://nixos.org/channels/nixos-unstable; #https://nixos.org/channels/nixos-17.09; #https://nixos.org/channels/nixos-17.03;
-
-  nixpkgs.config = {
-      allowUnfree = true;
-      allowBroken = true;
-      pulseaudio = true;
-      permittedInsecurePackages = [ "samba-3.6.25" ];
-      chromium = { enablePepperFlash = true;  };
-      overlays = [ (import /etc/nixos/overlays/nixpkgs-mozilla/rust-overlay.nix) ];
-  };
-
-  nix = {
-      buildCores = 0;
-      daemonIONiceLevel = 4;
-      daemonNiceLevel = 10;
-      maxJobs = 8; #4
-      gc.automatic = false;
-      gc.dates = "daily";
-      gc.options = "--delete-older-than 1h";
-      #nixPath = [ "/etc/nixos" "nixos-config=/etc/nixos/configuration.nix" ];
-      useSandbox = false; #nix.useChroot
-      binaryCaches = [ http://cache.nixos.org http://hydra.nixos.org "https://nixcache.reflex-frp.org" ];
-      binaryCachePublicKeys =  [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
-      trustedUsers =  [ "root" "domini" "@wheel" ];
-      extraOptions = ''
-              auto-optimise-store = true
-              gc-keep-outputs = false
-              gc-keep-derivations = false
-      '';
-  };
-
-
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
 
 }
-
--- packages.nix
+++ packages.nix
@@ -15,18 +15,16 @@ pkgs: with pkgs; [
     qt5.qtbase
     xfce.exo xfce.gtk_xfce_engine xfce.gvfs xfce.terminal xfce.thunar xfce.thunar_volman xfce.xfce4icontheme xfce.xfce4settings xfce.xfconf 
     xlibs.xev
-    xorg.xdpyinfo xorg.xkill xorg.xmodmap 
+    xorg.xdpyinfo xorg.xkill xorg.xmodmap xorg.xinit
     #sqldeveloper
-    
 
-    
     abduco abiword ack acpi adobe-reader amsn android-studio anki ansible ansible2 ant apparmor-pam apparmor-parser apparmor-profiles apparmor-utils arandr arduino aria2 asciinema aspell atom atool autoconf aws-auth awscli 
     axel babeltrace bar-xft bazaar bc bedup beets bind binutils bitcoin bitcoin-xt bitlbee bitlbee-facebook bitlbee-steam blink blueman bmon boot bridge-utils bundix bwm_ng cabal2nix calc calibre cargo cdrkit centerim 
     chromium cifs_utils citrix_receiver clang clerk clipit cloc cloud-init cmake cmus cnijfilter2 cntlm compton containerd coq coreutils corkscrew coyim cryptsetup ctags cups-bjnp curl cvs cvs_fast_export dapp darcs davmail 
     ddate debootstrap deluge desktop_file_utils devilspie2 dhcp dhcpcd di dia dino discord dmenu dmenu2 dmidecode dnsmasq docbook5 docbook_xml_xslt docker docker-machine docker-machine-kvm dos2unix dropbox dstat dunst 
     duperemove dvtm ebtables ekiga elementary-icon-theme elinks elixir emacs evince exiv2 expect fasd fdupes ffmpeg file filezilla firefox-beta-bin-unwrapped firefox-devedition-bin-unwrapped flac fop fortune franz freemind 
     freerdp freetalk fuse fzf gajim gcc gcc6 gdb ghostscriptX gimp gitFull glib global glxinfo gnucash gnumake go go2nix google-chrome google-cloud-sdk gpgme gpm gptfdisk gradle graphviz gtk2 gtk-recordmydesktop gtmess gtypist 
-    guile hdparm hevm hicolor_icon_theme hipchat hplip htop httpie i3 i3lock i3status icedtea_web iftop imagemagick inkscape iomelt iotop iptables irssi isync iw jackline jitsi jmtpfs jnettop jq kadu keepassx2 konsole kubernetes 
+    guile hdparm hevm hicolor_icon_theme hipchat htop httpie i3 i3lock i3status icedtea_web iftop imagemagick inkscape iomelt iotop iptables irssi isync iw jackline jitsi jmtpfs jnettop jq kadu keepassx2 konsole kubernetes 
     languagetool ledger leiningen libguestfs libinput libmysql libnotify libpcap libproxy libreoffice libsysfs libva libvirt libxml2 libxslt libyaml links linphone lr lsh lshw lsof ltrace lttng-tools lttng-ust lxc lynx maven mc 
     mcabber meld mercurialFull midori mikutter minikube mkpasswd mm mongodb-tools mosh mosquitto most mpv msmtp mtr nasm ncdu ncmpcpp ncurses neomutt neovim nethogs networkmanagerapplet networkmanager_openvpn nfs-utils nftables 
     nitrogen nixops nix-prefetch-git nix-prefetch-scripts nix-repl nixUnstable nmap nodejs notmuch nq nss nssTools ntfs3g ntopng okular openconnect openocd openssl openvswitch opera oysttyer p7zip packer pagemon pandoc paprefs 
@@ -35,7 +33,7 @@ pkgs: with pkgs; [
     rustracer s3cmd samba screen scribus scrot scudcloud seth shared_mime_info shellcheck silver-searcher simple-scan simplescreenrecorder skype skypeforlinux slack socat spotify  sqlite sshfsFuse sshpass stack 
     stalonetray steam stellar-core stow stunnel sublime3 subversion sxhkd sxiv sysdig sysstat system-config-printer taffybar tango-icon-theme taskwarrior tcpdump tensor terminator termite testdisk texstudio tmux torbrowser 
     toxic transmission_gtk tree turses tuxguitar tweak unclutter unrar unzip urlview usbutils utillinux utox vacuum vagrant valgrind vanilla-dmz vdpauinfo viber vifm vimHugeX virtmanager virt-viewer vis visualvm vlc vmtouch 
-    vnstat w3m-full watchman wayland weechat weechat-matrix-bridge weechat-xmpp wget which wine wineStaging winetricks wireshark-gtk wmctrl xautolock xclip xdg-user-dirs xdg_utils xdotool xfontsel xidel xlockmore xlsfonts 
+    vnstat w3m-full watchman wayland weechat weechat-matrix-bridge weechat-xmpp wget which wine wineStaging winetricks wmctrl xautolock xclip xdg-user-dirs xdg_utils xdotool xfontsel xidel xlockmore xlsfonts 
     xmpp-client xsane xsel xss-lock yakuake youtube-dl z3 zathura zeal zfs zfstools zip zoom-us zsh zsh-autosuggestions zsh-completions zsh-git-prompt zsh-navigation-tools zsh-prezto zsh-syntax-highlighting 
 
 
